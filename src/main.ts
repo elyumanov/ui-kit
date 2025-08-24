4b6aa8ca6aaf03baa4b209e672c6e45388159ddf
Lyumanov E.E <e.lumanov@kodit.dev>
2025-08-24 04:44:43 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "./style.css";
+
+// This file is only needed for Vite to work
+// The actual app will be Storybook
+const app = createApp({});
+app.mount("#app");
